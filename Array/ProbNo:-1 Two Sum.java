e5d7d4e43f4f83173f762a3bec403906a5542333
amansingh5135 <ak7342222991@gmail.com>
2025-02-25 23:13:26 +0530

    Create ProbNo:-1 Two Sum.java

---
+++ Array/ProbNo:-1 Two Sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        int n = arr.length;
+        for(int i-0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println(i,j);
+                }
+            }
+        }
+    }
+}
