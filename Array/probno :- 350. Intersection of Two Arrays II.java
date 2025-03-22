0eea8d702b90bd39d650916d527d00ea7749b94d
amansingh5135 <ak7342222991@gmail.com>
2025-03-22 22:23:13 +0530

    Update probno :- 350. Intersection of Two Arrays II.java

--- Array/probno :- 350. Intersection of Two Arrays II.java
+++ Array/probno :- 350. Intersection of Two Arrays II.java
@@ -1,14 +1,19 @@
 class Solution {
-    public boolean threeConsecutiveOdds(int[] arr) {
-        int count = 0;
-        for(int ele : arr){
-            if(ele%2==1){
-                count++;
-                if(count==3){
-                    return true;
+    public int[] intersect(int[] n, int[] m) {
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < n.length ; i++){
+            for(int j = 0; j < m.length ; j++){
+                if(n[i]==m[j]){
+                    result.add(m[j]);
+                    m[j] = -1;
+                    break;
                 }
-            } else count=0;
+             }
         }
-        return false;
+        int[] ans = new int[result.size()];
+        for(int i = 0 ; i < ans.length ; i++){
+            ans[i] = result.get(i);
+         }
+        return ans;
     }
 }
